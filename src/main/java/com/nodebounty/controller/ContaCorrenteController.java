dea2cccf4dcd27ae892de5e306f1d6f2a5a4628a
d4vwdi <d4vwdi@gmail.com>
2023-10-14 16:12:54 -0300

    Descrição dos métodos para fins didáticos

--- src/main/java/com/nodebounty/controller/ContaCorrenteController.java
+++ src/main/java/com/nodebounty/controller/ContaCorrenteController.java
@@ -28,15 +28,24 @@ public class ContaCorrenteController {
 
     @PostMapping
     public String processarEscolhaPlano(@RequestParam(name = "plano", required = false) String planoSelecionado) {
+
+            //Esta linha verifica se a variável planoSelecionado não é nula (null) e se não está vazia.
+            // Em outras palavras, estamos verificando se o usuário selecionou algum plano antes de continuar com o processamento.
         if (planoSelecionado != null && !planoSelecionado.isEmpty()) {
-            // Aqui você pode buscar o objeto Plano correspondente ao plano selecionado
+
+            // Nesta linha, estamos usando o planoRepository (um repositório JPA) para buscar um objeto Plano com base no nome do plano selecionado.
+            // O método findByNomePlano é personalizado no repositório e permite encontrar um plano com o nome correspondente ao planoSelecionado.
             Plano plano = planoRepository.findByNomePlano(planoSelecionado);
 
-            // Crie uma instância de ContaCorrente e associe o plano selecionado
+            // Aqui estamos criando uma nova instância da classe Contacorrente.
+            // Esta instância representará a conta corrente que será associada ao plano selecionado.
             Contacorrente contaCorrente = new Contacorrente();
+            // Nesta linha, estamos associando o plano selecionado à conta corrente que acabamos de criar.
+            // Isso significa que a conta corrente será vinculada ao plano escolhido pelo usuário.
             contaCorrente.setPlanosConta(plano);
 
-            // Salve a ContaCorrente no banco de dados
+            // Por fim, estamos usando o contacorrenteRepository para salvar a conta corrente no banco de dados.
+            // Isso efetivamente cria um registro de conta corrente no banco de dados com o plano selecionado pelo usuário.
             contacorrenteRepository.save(contaCorrente);
         }
         return null;
--- src/main/java/com/nodebounty/domain/plano/PlanoRepository.java
+++ src/main/java/com/nodebounty/domain/plano/PlanoRepository.java
@@ -8,10 +8,24 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@Repository //Esta anotação é usada para indicar que a interface PlanoRepository é um componente gerenciado pelo Spring, especificamente um repositório de dados.
+            // Os repositórios são usados para acessar e manipular dados do banco de dados de maneira conveniente.
+
+
+// Esta é a declaração da interface PlanoRepository, que estende JpaRepository. O JpaRepository é parte do Spring Data JPA e fornece
+// métodos convenientes para realizar operações de banco de dados, como salvar, buscar, atualizar e excluir registros.
+// Neste caso, ele está sendo usado para trabalhar com entidades do tipo Plano com identificadores do tipo Long.
 public interface PlanoRepository extends JpaRepository<Plano, Long> {
 
     // Consulta personalizada para buscar planos por nome
-    @Query("SELECT p FROM Plano p WHERE p.nomePlano = :nomes")
+    @Query("SELECT p FROM Plano p WHERE p.nomePlano = :nomes")//@Query é usada para definir uma consulta personalizada em JPQL (Java Persistence Query Language).
+    //Neste caso, a consulta seleciona todos os registros da entidade Plano onde o atributo nomePlano corresponde ao parâmetro :nomes.
+    // O :nomes é um parâmetro nomeado que será vinculado ao parâmetro do método a seguir.
+
+    // Este é um método de consulta personalizado que corresponde à consulta JPQL definida anteriormente.
+    // Ele recebe um parâmetro String chamado nome, que será correspondido ao parâmetro nomeado :nomes na consulta.
+    // O método retornará um objeto Plano com base no resultado da consulta. O nome do método segue uma convenção de
+    // nomenclatura especial do Spring Data JPA que usa a palavra "findBy" seguida pelo nome do atributo da entidade (nomePlano no caso)
+    // que está sendo consultado.
     Plano findByNomePlano(@Param("nomes") String nome);
 }
