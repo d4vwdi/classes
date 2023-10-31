5dd8624a3db55a472ee31805b4f1aaa67769f76d
d4vwdi <d4vwdi@gmail.com>
2023-10-30 21:10:27 -0300

    Finalizando a página de transação

--- src/main/java/com/nodebounty/controller/TransacaoController.java
+++ src/main/java/com/nodebounty/controller/TransacaoController.java
@@ -2,18 +2,21 @@ package com.nodebounty.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nodebounty.domain.contacorrente.Contacorrente;
 
 @Controller
 
 public class TransacaoController {
 
     @GetMapping("/transacao")
-    public String transacaoPage() {
+    public String transacaoPage(Model model) {
+        Contacorrente conta1 = new Contacorrente();
+        conta1.setSaldoConta(180.00);
+        
+        model.addAttribute("conta1", conta1);
+        
         return "transacao";
     }
 
